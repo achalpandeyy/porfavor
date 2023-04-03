28c307cb11f526813415795d241130c16684cf81
achalpandeyy <pandeyachal.ap@gmail.com>
2023-04-03 21:18:30 +0530

    Handle the MOD=00 mov, with and without the special direct address case.

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,10 @@ int main(int argc, char** argv)
 
     while (code_ptr != assembled_code.data() + assembled_code.size())
     {
+        uint64_t instruction_size = 0;
+
+        output_stream << "mov ";
+
         if (((code_ptr[0] >> 2) & std::byte{0b111111}) == std::byte{0b100010}) // Register/memory to/from register
         {
             // If D = 0, then REG gives the src register and R/M gives the dst register.
@@ -69,89 +73,109 @@ int main(int argc, char** argv)
             const std::byte D = (code_ptr[0] >> 1) & std::byte{0b1};
             const std::byte W = (code_ptr[0] & std::byte{0b1});
             const std::byte MOD = (code_ptr[1] >> 6) & std::byte{0b11};
+            const std::byte REG = (code_ptr[1] >> 3) & std::byte{0b111};
+            const std::byte R_M = code_ptr[1] & std::byte{0b111};
+
+            auto insert_register_name = [&output_stream](const std::byte predicate, const std::byte w) -> bool
+            {
+                if (predicate == std::byte{0b000})
+                    w == std::byte{0b0} ? output_stream << "al" : output_stream << "ax";
+                else if (predicate == std::byte{0b001})
+                    w == std::byte{0b0} ? output_stream << "cl" : output_stream << "cx";
+                else if (predicate == std::byte{0b010})
+                    w == std::byte{0b0} ? output_stream << "dl" : output_stream << "dx";
+                else if (predicate == std::byte{0b011})
+                    w == std::byte{0b0} ? output_stream << "bl" : output_stream << "bx";
+                else if (predicate == std::byte{0b100})
+                    w == std::byte{0b0} ? output_stream << "ah" : output_stream << "sp";
+                else if (predicate == std::byte{0b101})
+                    w == std::byte{0b0} ? output_stream << "ch" : output_stream << "bp";
+                else if (predicate == std::byte{0b110})
+                    w == std::byte{0b0} ? output_stream << "dh" : output_stream << "si";
+                else if (predicate == std::byte{0b111})
+                    w == std::byte{0b0} ? output_stream << "bh" : output_stream << "di";
+                else
+                    return false;
+
+                return true;
+            };
+
             if (MOD == std::byte{0b00})
             {
+                instruction_size += 2;
 
-            }
-            else if (MOD == std::byte{0b01})
-            {
+                uint16_t direct_address = UINT16_MAX; // Assuming this won't be a direct address, but it could be, who knows.
 
-            }
-            else if (MOD == std::byte{0b10})
-            {
-
-            }
-            else if (MOD == std::byte{0b11})
-            {
-                const std::byte REG = (code_ptr[1] >> 3) & std::byte{0b111};
-                const std::byte R_M = code_ptr[1] & std::byte{0b111};
-
-                output_stream << "mov ";
-
-                // NOTE: It just so happens that for MOD 11 (register-to-register mode) both REG and R/M fields are mapped to the same register names.
-                auto insert_register_name = [&output_stream, W](const std::byte predicate) -> bool
+                if (R_M == std::byte{0b110})
                 {
-                    if (predicate == std::byte{0b000})
-                        W == std::byte{0b0} ? output_stream << "al" : output_stream << "ax";
-                    else if (predicate == std::byte{0b001})
-                        W == std::byte{0b0} ? output_stream << "cl" : output_stream << "cx";
-                    else if (predicate == std::byte{0b010})
-                        W == std::byte{0b0} ? output_stream << "dl" : output_stream << "dx";
-                    else if (predicate == std::byte{0b011})
-                        W == std::byte{0b0} ? output_stream << "bl" : output_stream << "bx";
-                    else if (predicate == std::byte{0b100})
-                        W == std::byte{0b0} ? output_stream << "ah" : output_stream << "sp";
-                    else if (predicate == std::byte{0b101})
-                        W == std::byte{0b0} ? output_stream << "ch" : output_stream << "bp";
-                    else if (predicate == std::byte{0b110})
-                        W == std::byte{0b0} ? output_stream << "dh" : output_stream << "si";
-                    else if (predicate == std::byte{0b111})
-                        W == std::byte{0b0} ? output_stream << "bh" : output_stream << "di";
+                    direct_address = (static_cast<uint16_t>(code_ptr[3]) << 8) | static_cast<uint16_t>(code_ptr[2]);
+                    instruction_size += 2; // add the bytes for encoding displacement
+                }
+
+                auto insert_address_calculation = [&output_stream, direct_address](const std::byte R_M_) -> bool
+                {
+                    if (R_M_ == std::byte{0b000})
+                        output_stream << "[bx + si]";
+                    else if (R_M_ == std::byte{0b001})
+                        output_stream << "[bx + di]";
+                    else if (R_M_ == std::byte{0b010})
+                        output_stream << "[bp + si]";
+                    else if (R_M_ == std::byte{0b011})
+                        output_stream << "[bp + di]";
+                    else if (R_M_ == std::byte{0b100})
+                        output_stream << "[si]";
+                    else if (R_M_ == std::byte{0b101})
+                        output_stream << "[di]";
+                    else if (R_M_ == std::byte{0b110})
+                        output_stream << "[" << direct_address << "]";
+                    else if (R_M_ == std::byte{0b111})
+                        output_stream << "[bx]";
                     else
                         return false;
-
+                    
                     return true;
                 };
 
-                if (D == std::byte{0b0})
+                if (!((D == std::byte{0b0}) ? insert_address_calculation(R_M) : insert_register_name(REG, W)))
                 {
-                    if (!insert_register_name(R_M))
-                    {
-                        std::cout << "Invalid R/M value." << std::endl;
-                        return -1;
-                    }
-
-                    output_stream << ", ";
-
-                    if (!insert_register_name(REG))
-                    {
-                        std::cout << "Invalid REG value." << std::endl;
-                        return -1;
-                    }
-                }
-                else if (D == std::byte{0b1})
-                {
-                    if (!insert_register_name(REG))
-                    {
-                        std::cout << "Invalid REG value." << std::endl;
-                        return -1;
-                    }
-
-                    output_stream << ", ";
-
-                    if (!insert_register_name(R_M))
-                    {
-                        std::cout << "Invalid R/M value." << std::endl;
-                        return -1;
-                    }
-                }
-                else
-                {
-                    std::cout << "[ERROR]: Invalid D value." << std::endl;
+                    std::cout << "[ERROR]: Invalid " << ((D == std::byte{0b0}) ? "R/M" : "REG") << " value." << std::endl;
                     return -1;
                 }
 
-                output_stream << "\n";
+                output_stream << ", ";
+
+                if (!((D == std::byte{0b1}) ? insert_address_calculation(R_M) : insert_register_name(REG, W)))
+                {
+                    std::cout << "[ERROR]: Invalid " << ((D == std::byte{0b1}) ? "R/M" : "REG") << " value." << std::endl;
+                    return -1;
+                }
+            }
+            else if (MOD == std::byte{0b01})
+            {
+                // 8-bit displacement.
+            }
+            else if (MOD == std::byte{0b10})
+            {
+                // 16-bit displacement.
+            }
+            else if (MOD == std::byte{0b11})
+            {
+                // NOTE: It just so happens that for MOD 11 (register-to-register mode) both REG and R/M fields are mapped to the same register names.
+                assert(D == std::byte{0b0} || D == std::byte{0b1});
+
+                if (!insert_register_name((D == std::byte{0b0} ? R_M : REG), W))
+                {
+                    std::cout << "Invalid " << ((D == std::byte{0b0}) ? "R/M" : "REG") << " value." << std::endl;
+                    return -1;
+                }
+
+                output_stream << ", ";
+
+                if (!insert_register_name((D == std::byte{0b1}) ? R_M : REG, W))
+                {
+                    std::cout << "Invalid " << (D == std::byte{0b1} ? "R/M" : "REG") << " value." << std::endl;
+                    return -1;
+                }
             }
             else
             {
@@ -181,7 +205,8 @@ int main(int argc, char** argv)
             return -1;
         }
 
-        code_ptr += 2;
+        output_stream << "\n";
+        code_ptr += instruction_size;
     }
 
     const char* out_file_path;
