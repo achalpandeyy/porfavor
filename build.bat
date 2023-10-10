d4b50df6cf0e1aef98fb4cf0836e8271460443eb
achalpandeyy <pandeyachal.ap@gmail.com>
2023-10-10 10:10:34 +0530

    Implement "clustered" sampling for haversine input generation.

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ pushd build
 set COMPILER_FLAGS=-nologo -W4 -WX -wd4100 -MP -Zi -I../ext
 
 cl %COMPILER_FLAGS% /Od -Fe:porfavor ../src/main.c
-cl %COMPILER_FLAGS% -D_CRT_SECURE_NO_WARNINGS /O2 -Fe:haversine ../src/haversine.cpp
+cl %COMPILER_FLAGS% -D_CRT_SECURE_NO_WARNINGS /O2 -Fe:haversine_generator ../src/haversine_generator.cpp
 
 echo Build complete.
 
--- src/haversine_generator.cpp
+++ src/haversine_generator.cpp
@@ -1,3 +1,4 @@
+// TODO(achal): Rename this file: haversine_generator.cpp because it is a generator of data.
 #include <stdio.h>
 #include <math.h>
 #include <stdint.h>
@@ -61,9 +62,11 @@ static u64 parse_u64_from_string(const char *num_string)
 
 int main(int argc, char **argv)
 {
+    u64 max_pair_count = 10000000;
+    
     if (argc != 4)
     {
-        fprintf(stdout, "Usage: haversine.exe [uniform] [seed] [number of pairs]\n");
+        fprintf(stdout, "Usage: haversine_generator.exe [uniform | clustered] [seed] [number of pairs <= %llu]\n", max_pair_count);
         return -1;
     }
     
@@ -73,16 +76,37 @@ int main(int argc, char **argv)
         fprintf(stdout, "WARNING: Failed to open file %s\n", json_path);
     
     char *method_name = argv[1];
-    assert(strcmp(method_name, "uniform") == 0);
+    
+    u32 cluster_count_x;
+    u32 cluster_count_y;
+    if (strcmp(method_name, "clustered") == 0)
+    {
+        cluster_count_x = 16;
+        cluster_count_y = 16;
+    }
+    else if (strcmp(method_name, "uniform") == 0)
+    {
+        cluster_count_x = 1;
+        cluster_count_y = 1;
+    }
+    else
+    {
+        fprintf(stderr, "ERROR: Invalid sampling method: %s\n", method_name);
+        return -1;
+    }
+    
     u64 seed = parse_u64_from_string(argv[2]);
     u64 pair_count = parse_u64_from_string(argv[3]);
+    if (pair_count > max_pair_count)
+    {
+        fprintf(stderr, "Max pair count exceeded\n");
+        return -1;
+    }
     
     fprintf(stdout, "Method: %s\n", method_name);
     fprintf(stdout, "Seed: %llu\n", seed);
     fprintf(stdout, "Pair count: %llu\n", pair_count);
     
-    std::uniform_real_distribution<f64> dist_x(-180.0, 180.0);
-    std::uniform_real_distribution<f64> dist_y(-90.f, 90.f);
     std::mt19937_64 prng(seed);
     
     char answers_path[] = "haversine_answers.f64";
@@ -96,24 +120,59 @@ int main(int argc, char **argv)
     
     f64 average = 0.0;
     
-    for (u64 i = 0; i < pair_count; ++i)
+    u64 total_cluster_count = cluster_count_x * cluster_count_y;
+    u64 pairs_per_cluster = (max_pair_count+total_cluster_count-1)/total_cluster_count;
+    
+    f64 x_min = -180.0;
+    f64 x_max = 180.0;
+    f64 y_min = -90.0;
+    f64 y_max = 90.0;
+    
+    f64 x_span = x_max-x_min;
+    f64 y_span = y_max-y_min;
+    
+    f64 x_cluster_span = x_span/cluster_count_x;
+    f64 y_cluster_span = y_span/cluster_count_y;
+    
+    u64 pairs_picked = 0;
+    
+    for (u32 y_cluster = 0; y_cluster < cluster_count_y; ++y_cluster)
     {
-        f64 x0 = dist_x(prng);
-        f64 y0 = dist_y(prng);
-        f64 x1 = dist_x(prng);
-        f64 y1 = dist_y(prng);
-        
-        f64 haversine_distance = ReferenceHaversine(x0, y0, x1, y1, earth_radius);
-        if (answers_file)
-            fwrite(&haversine_distance, sizeof(f64), 1, answers_file);
-        
-        average += haversine_distance;
-        
-        fprintf(json_file, "\t{x0: \"%.18f\", y0: \"%.18f\", x1: \"%.18f\", y1: \"%.18f\"}", x0, y0, x1, y1);
-        if (i != (pair_count-1))
-            fprintf(json_file, ",");
-        fprintf(json_file, "\n");
+        std::uniform_real_distribution<f64> dist_y(y_min + y_cluster*y_cluster_span, y_min + (y_cluster+1)*y_cluster_span);
+        for (u32 x_cluster = 0; x_cluster < cluster_count_x; ++x_cluster)
+        {
+            std::uniform_real_distribution<f64> dist_x(x_min + x_cluster*x_cluster_span, x_min + (x_cluster+1)*x_cluster_span);
+            
+            for (u32 pair_idx = 0; pair_idx < pairs_per_cluster; ++pair_idx)
+            {
+                f64 x0 = dist_x(prng);
+                f64 y0 = dist_y(prng);
+                f64 x1 = dist_x(prng);
+                f64 y1 = dist_y(prng);
+                
+                f64 haversine_distance = ReferenceHaversine(x0, y0, x1, y1, earth_radius);
+                if (answers_file)
+                    fwrite(&haversine_distance, sizeof(f64), 1, answers_file);
+                
+                average += haversine_distance;
+                
+                fprintf(json_file, "\t{x0: \"%.18f\", y0: \"%.18f\", x1: \"%.18f\", y1: \"%.18f\"}", x0, y0, x1, y1);
+                
+                pairs_picked++;
+                assert(pairs_picked <= pair_count);
+                
+                if ((pair_count-pairs_picked) >= 1)
+                    fprintf(json_file, ",");
+                fprintf(json_file, "\n");
+                
+                if (pairs_picked == pair_count)
+                    goto end_picking;
+            }
+        }
     }
+    
+    end_picking:
+    assert(pairs_picked == pair_count);
     fprintf(json_file, "]}\n");
     
     average /= pair_count;
